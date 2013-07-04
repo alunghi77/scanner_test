bef50f91e7fbfa2c1decc24349f7ea645ec14b14
Alessio Lunghi <alessio@gamaroff.co.uk>
2013-07-04 12:37:28 +0100

    ping google

--- test.py
+++ test.py
@@ -4,6 +4,17 @@ from time import sleep
 from Adafruit_I2C import Adafruit_I2C
 from Adafruit_MCP230xx import Adafruit_MCP230XX
 from Adafruit_CharLCDPlate import Adafruit_CharLCDPlate
+from subprocess import call
+
+import smbus
+import subprocess
+import re
+import socket
+import fcntl
+import struct
+import paramiko
+import socket
+import signal
 
 lcd = Adafruit_CharLCDPlate(busnum = 1)
 
@@ -31,6 +42,36 @@ def init_test():
 	lcd.backlight(lcd.BLUE)
 	lcd.message("Welcome to \nGD Scanner Unit ")
 	sleep(21)
+
+	# ---------------------
+	# | Ping System Check |
+	# ---------------------
+ 
+	# Put stderr and stdout into pipes
+	proc = subprocess.Popen("ping -c 2 google.com 2>&1", \
+			shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+ 
+	return_code = proc.wait()
+ 
+	# Read from pipes
+	# stdout
+	for line in proc.stdout:
+		if "loss" in line:
+			packet_loss = progress = re.search('\d*%',line).group()
+			if int(packet_loss.split('%')[0]) > 0:
+				lcd.clear()
+				lcd.backlight(lcd.RED)
+				lcd.message("Ping Google:\nFailed")
+				sleep(1)
+				#print packet_loss + " packet loss."
+			else:
+				lcd.clear()
+				lcd.backlight(lcd.GREEN)
+				lcd.message("Ping Google:\nSuccess")
+				sleep(1)
+	#stderr
+	for line in proc.stderr:
+		print("stderr: " + line.rstrip())
 
 
 # Start the on board system check
