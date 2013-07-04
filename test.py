4949a146df63e2062c632144a45c587f5d83a1f5
Alessio Lunghi <alessio@gamaroff.co.uk>
2013-07-04 13:59:38 +0100

    ping google

--- test.py
+++ test.py
@@ -73,7 +73,88 @@ def init_test():
 	for line in proc.stderr:
 		print("stderr: " + line.rstrip())
 
-
+	# --------------------
+	# | Ping Default GW  |
+	# --------------------
+	ip_gateway = get_gateway(iface)
+ 
+	proc = subprocess.Popen ("ping -c 2 " + ip_gateway + " 2>&1", \
+			shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+ 
+	return_code = proc.wait()
+ 
+	# Read from pipes
+	# stdout
+	for line in proc.stdout:
+	   if "loss" in line:
+		   packet_loss = re.search('\d*%',line).group()
+		   if int(packet_loss.split('%')[0]) > 0:
+			   lcd.clear()
+			   lcd.backlight(lcd.RED)
+			   lcd.message("Ping Gateway:\nFailed")
+			   sleep(1)
+			   #print ip_gateway + packet_loss + " packet loss for gateway." 
+		   else:
+			   lcd.clear()
+			   lcd.backlight(lcd.GREEN)
+			   lcd.message("Ping Gateway:\nSuccess")
+			   sleep(1)
+			   #print "Gateway is reachable"  
+	# stderr
+	for line in proc.stderr:
+		print("stderr: " + line.rstrip())
+ 
+	# --------------------
+	# | DHCP IP Address  |
+	# --------------------
+ 
+	try :
+		ip_address = get_ip_address(iface)
+		lcd.clear()
+		lcd.backlight(lcd.GREEN)
+		lcd.message("IP:\n" + ip_address)
+		sleep(1)
+	except :
+		lcd.clear()
+		lcd.backlight(lcd.RED)
+		lcd.message("No IP obtained")
+		sleep(1)
+ 
+	# -------------------
+	# |  Reverse Shell  |
+	# -------------------
+	try:
+		ssh = paramiko.SSHClient()
+		ssh.load_system_host_keys()
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		ssh.connect(ccIP, username='twi7ch', password='none')
+	except paramiko.AuthenticationException:
+		lcd.clear()
+		lcd.backlight(lcd.GREEN)
+		lcd.message("Reverse Tunnel:\nSuccess")
+		sleep(1)
+	except socket.error:
+		lcd.clear()
+		lcd.message("Reverse Shell: \nFailed")
+		lcd.backlight(lcd.RED)
+		sleep(1)
+ 
+	# Do we want to rerun the test?
+	lcd.clear()
+	lcd.backlight(lcd.YELLOW)
+	lcd.message("Run test again?")
+	sleep(1)
+	lcd.clear()
+ 
+	lcd.message("Yes = Left Btn\nNo = Right Btn")
+	signal.signal(signal.SIGALRM, timeout)
+ 
+	#change 5 to however many seconds you need
+	signal.alarm(10)
+	try:
+		main()
+	except TimeoutException:
+		exit()
 # Start the on board system check
 init_test()			
  
