2bb89b4746b0ca0587b1785424ae7435d054eca5
Alessio Lunghi <alessio@gamaroff.co.uk>
2013-07-05 16:33:56 +0100

    testing

--- test.py
+++ test.py
@@ -80,14 +80,19 @@ def do_get_temp():
 	
 
 def do_get_serial():
+
   	# Extract serial from cpuinfo file
   	cpuserial = "0000000000000000"
+
   	try:
-    	f = open('/proc/cpuinfo','r')
+
+		f = open('/proc/cpuinfo','r')
     	for line in f:
+
       		if line[0:6]=='Serial':
         		cpuserial = line[10:26]
-    		f.close()
+    	f.close()
+    	
   	except:
 		cpuserial = "ERROR000000000"
 
